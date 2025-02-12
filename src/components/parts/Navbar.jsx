00ac783201d7f125f1743bc0b613c6891cc3da32
Muhammad Delowar Hosen MI <md48735002@gmail.com>
2025-02-12 23:26:51 +0600

    refactor: adjust Header and Navbar components for improved responsiveness and styling

--- src/components/parts/Navbar.jsx
+++ src/components/parts/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
 
   useEffect(() => {
     function size() {
-      if (window.innerWidth < 1024) {
-        setShow(false);
+      if (window.innerWidth >= 1024) {
+        setShow(true); // lg এর জন্য সবসময় দেখাবে
       } else {
-        setShow(true);
+        setShow(false);
       }
     }
     size();
@@ -23,47 +23,39 @@ const Navbar = () => {
   }, []);
 
   let toggleMenu = () => {
-    setShow(!show);
+    setShow((prev) => !prev);
   };
 
   return (
     <div className="w-full py-3 bg-slate-200 top-0 sticky z-50">
       <Container>
-        <Flex className={"justify-between"}>
+        <Flex className="justify-between">
           {/* Logo */}
           <div className="w-[40%] translate-y-1/3">
             <Link to="/">
-              <Image ImgSrc={Logo} className={"pl-2"} />
+              <Image ImgSrc={Logo} className="pl-2" />
             </Link>
           </div>
 
           {/* Navbar Menu */}
           <div
-            className={`transition-all duration-500 z-50 py-2 lg:w-[60%] w-full left-0 lg:flex absolute top-[50px] bg-red-300 lg:bg-transparent lg:static ${
-              show ? "opacity-100 visible" : "opacity-0 invisible"
+            className={`transition-all duration-500 z-50 py-2 lg:w-[60%] w-full left-0 lg:flex absolute lg:static bg-red-300 lg:bg-transparent ${
+              show ? "opacity-100 visible top-[50px]" : "opacity-0 invisible -top-full lg:opacity-100 lg:visible lg:static"
             }`}
           >
             <ul>
-              <Flex className={"gap-x-10 gap-y-2 justify-center flex-col text-center lg:flex-row"}>
-                <Link to="/" onClick={() => setShow(false)}>
-                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">
-                    Home
-                  </li>
+              <Flex className="gap-x-10 gap-y-2 justify-center flex-col text-center lg:flex-row">
+                <Link to="/" onClick={() => window.innerWidth < 1024 && setShow(false)}>
+                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">Home</li>
                 </Link>
-                <Link to="/products" onClick={() => setShow(false)}>
-                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">
-                    Shop
-                  </li>
+                <Link to="/products" onClick={() => window.innerWidth < 1024 && setShow(false)}>
+                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">Shop</li>
                 </Link>
-                <Link to="/about" onClick={() => setShow(false)}>
-                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">
-                    About
-                  </li>
+                <Link to="/about" onClick={() => window.innerWidth < 1024 && setShow(false)}>
+                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">About</li>
                 </Link>
-                <Link to="/contact" onClick={() => setShow(false)}>
-                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">
-                    Contact
-                  </li>
+                <Link to="/contact" onClick={() => window.innerWidth < 1024 && setShow(false)}>
+                  <li className="font-dm text-navColor hover:text-navHColor hover:font-bold">Contact</li>
                 </Link>
               </Flex>
             </ul>
@@ -78,3 +70,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
